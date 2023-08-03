784325e41aa7f605d555d27e54dc1ce8de35843e
Rizwan Ahamad <rizwanahamadcodes@gmail.com>
2023-08-03 17:16:20 +0545

    shifting from scss to tailwind cause it makes sense for a component library

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
-import styles from "../styles/main.module.scss";
 import Header from "./Header";
+import styles from "@/styles/main.module.scss";
 
 export default function Home() {
     return (
         <>
             <Header />
-            <h1 className={styles["hi-there"]}>Hi! i was styled by scss</h1>
+            <h1 className={styles["heading"]}>Hi! i was styled by scss</h1>
         </>
     );
 }
