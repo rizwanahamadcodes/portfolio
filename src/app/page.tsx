8971b1800164abdbb434d5d989f13e89b6b02fb0
Rizwan Ahamad <rizwanahamadcodes@gmail.com>
2023-08-18 00:21:49 +0545

    container requires children so removed it from places where it is not being used

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,5 +3,5 @@ import harry from '../../public/img/harry.jpg'
 import Container from '@/components/Container'
 
 export default function Home() {
-    return <Container optionalStyles="mt-[60px]"></Container>
+    return <></>
 }
