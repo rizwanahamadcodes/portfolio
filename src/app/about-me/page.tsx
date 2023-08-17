5ed0935bd354e27c77fff60dd96079d50c973732
Rizwan Ahamad <rizwanahamadcodes@gmail.com>
2023-08-18 00:49:22 +0545

    minor tweaks

--- src/app/about-me/page.tsx
+++ src/app/about-me/page.tsx
@@ -2,11 +2,7 @@ import Container from '@/components/Container'
 import React from 'react'
 
 const page = () => {
-    return (
-        <Container optionalStyles="mt-[60px]">
-            <>About Me</>
-        </Container>
-    )
+    return <></>
 }
 
 export default page
