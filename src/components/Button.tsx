b78c66274926d298933b61a0b687a44d183140eb
Rizwan Ahamad <rizwanahamadcodes@gmail.com>
2023-08-21 12:19:31 +0545

    hero-section first draft

---
+++ src/components/Button.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+interface ButtonProps {
+    children: React.ReactNode
+}
+
+const Button: React.FC<ButtonProps> = (props) => {
+    const { children } = props
+
+    return <button>{children}</button>
+}
+
+export default Button
