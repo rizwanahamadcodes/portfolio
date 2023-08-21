2289f32863c21317c24bb7f7a1aea13648947696
Rizwan Ahamad <rizwanahamadcodes@gmail.com>
2023-08-21 22:28:14 +0545

    changing the color scheme

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,19 +1,11 @@
-import React from "react";
-import { FaPhoneAlt } from "react-icons/fa";
+import React from 'react'
 
 interface ButtonProps {
-    children: React.ReactNode;
-    gradient: string;
+    children: React.ReactNode
 }
 
-const Button: React.FC<ButtonProps> = ({ children, gradient }) => {
-    return (
-        <button
-            className={`${gradient} flex h-12 items-center gap-4 rounded-full bg-gradient-to-r px-4 text-gray-100  bg-blend-overlay transition-all duration-[0.1s] hover:bg-gray-100/10 hover:shadow-gray-900/50 first-line:hover:shadow active:scale-95 active:bg-gray-900/10`}>
-            <p>this was added on the phone</p>
-            {children}
-        </button>
-    );
-};
+const Button = () => {
+    return <div>Button</div>
+}
 
-export default Button;
+export default Button
