4e85b8e07b50040ca4c321a2a0679756c9f30b2c
Rizwan Ahamad <rizwanahamadcodes@gmail.com>
2023-08-21 15:58:24 +0545

    getting somewhere with the buttons

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -9,7 +9,7 @@ interface ButtonProps {
 const Button: React.FC<ButtonProps> = ({ children, gradient }) => {
     return (
         <button
-            className={`${gradient} flex h-12 items-center gap-4 rounded-full bg-gradient-to-r px-4 text-gray-100  bg-blend-overlay transition-all hover:bg-gray-100/10 hover:shadow hover:shadow-gray-900/50 active:bg-gray-900/10`}
+            className={`${gradient} flex h-12 items-center gap-4 rounded-full bg-gradient-to-r px-4 text-gray-100  bg-blend-overlay transition-all duration-[0.1s] hover:bg-gray-100/10 hover:shadow-gray-900/50 first-line:hover:shadow active:scale-95 active:bg-gray-900/10`}
         >
             {children}
         </button>
