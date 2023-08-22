28750bac03aff287c3ed99d240291a60e6c4a6c4
Rizwan Ahamad <rizwanahamadcodes@gmail.com>
2023-08-23 01:40:34 +0545

    minor change to the container

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -1,12 +1,9 @@
-import { useState } from 'react'
-
 interface ContainerProps extends React.HTMLProps<HTMLDivElement> {
     children: React.ReactNode
 }
 
 const Container = (props: ContainerProps) => {
     const { children, className, ...rest } = props
-    const [test, setTest] = useState(false)
 
     const containerClasses = `m-auto w-[86%] max-w-7xl ${className}`
 
