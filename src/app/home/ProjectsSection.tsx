7619ef7cfc24f5c157221a0514ce72d74f7c6fe2
Rizwan Ahamad <rizwanahamadcodes@gmail.com>
2023-10-12 21:18:43 +0545

    started work on the projects section

---
+++ src/app/home/ProjectsSection.tsx
@@ -0,0 +1,14 @@
+import Section from '@/components/Section'
+
+const ProjectsSection = () => {
+    return (
+        <Section
+            sectionClassName="bg-gray-200 dark:bg-gray-800"
+            containerClassName=""
+            title="Projects i've worked on"
+            subtitle="Every project featuring the usage of some of the most important front end tools"
+        ></Section>
+    )
+}
+
+export default ProjectsSection
