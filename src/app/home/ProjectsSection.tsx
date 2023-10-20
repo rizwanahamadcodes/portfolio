9fd6e2cc6fadb7778dcc3fb184a2b2218371678a
Rizwan Ahamad <rizwanahamadcodes@gmail.com>
2023-10-20 19:11:04 +0545

    added gray-50 and dark:gray-850 as backgrounds

--- src/app/home/ProjectsSection.tsx
+++ src/app/home/ProjectsSection.tsx
@@ -69,7 +69,7 @@ const Project = (props: ProjectProps) => {
     return (
         <div
             className={cn(
-                'group flex flex-col overflow-hidden rounded-2xl p-3 shadow-soft hover:shadow sm:flex-row-reverse',
+                'group flex flex-col overflow-hidden rounded-2xl bg-gray-50 p-3 shadow-soft hover:shadow dark:bg-gray-850 sm:flex-row-reverse',
                 positionInParent % 2 === 0 && 'sm:flex-row'
             )}
         >
