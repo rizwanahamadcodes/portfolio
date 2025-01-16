05891d8de7f7ade83d55dc7b167c8847d46f41bb
Rizwan Ahamad <rizwanahamadcodes@gmail.com>
2025-01-16 14:18:48 +0545

    going to use next/fonts cause it provies fonts optimizations

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,10 @@
 import Header from "./Header";
-import styles from "@/styles/main.module.scss";
 
 export default function Home() {
     return (
         <>
             <Header />
-            <h1 className={styles["heading"]}>Hi! i was styled by scss</h1>
+            <h1>Hi! i was styled by scss</h1>
         </>
     );
 }
