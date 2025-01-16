9e25d99e34ff2591bf83a6efde90ce7ad660aee6
Rizwan Ahamad <rizwanahamadcodes@gmail.com>
2025-01-16 14:18:48 +0545

    the navMenu has a direction property now, can be used as row and column

---
+++ src/app/about-me/page.tsx
@@ -0,0 +1,12 @@
+import Container from '@/components/Container'
+import React from 'react'
+
+const page = () => {
+    return (
+        <Container optionalStyles="mt-[60px]">
+            <>About Me</>
+        </Container>
+    )
+}
+
+export default page
