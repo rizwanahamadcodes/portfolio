d836c63b0f8fbb700af77b472ac73724f324c915
Rizwan Ahamad <rizwanahamadcodes@gmail.com>
2025-01-16 14:18:51 +0545

    edited and added the hero image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,18 @@
 import Image from 'next/image'
 import harry from '../../public/img/harry.jpg'
 import Container from '@/components/Container'
+import ThemedImage from '@/components/ThemedImage'
+import heroImageLight from '../../public/img/hero_image_light.svg'
+import heroImageDark from '../../public/img/hero_image_dark.svg'
 
 export default function Home() {
-    return <></>
+    return (
+        <>
+            <ThemedImage
+                darkImageSrc={heroImageDark}
+                lightImageSrc={heroImageLight}
+                alt="Hero image"
+            />
+        </>
+    )
 }
