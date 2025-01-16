a77124d3ee3ee2f35283019a24f9db54caec16ea
Rizwan Ahamad <rizwanahamadcodes@gmail.com>
2025-01-16 14:18:54 +0545

    added a paragraph from the phone

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,19 +1,19 @@
-import React from 'react'
-import { FaPhoneAlt } from 'react-icons/fa'
+import React from "react";
+import { FaPhoneAlt } from "react-icons/fa";
 
 interface ButtonProps {
-    children: React.ReactNode
-    gradient: string
+    children: React.ReactNode;
+    gradient: string;
 }
 
 const Button: React.FC<ButtonProps> = ({ children, gradient }) => {
     return (
         <button
-            className={`${gradient} flex h-12 items-center gap-4 rounded-full bg-gradient-to-r px-4 text-gray-100  bg-blend-overlay transition-all duration-[0.1s] hover:bg-gray-100/10 hover:shadow-gray-900/50 first-line:hover:shadow active:scale-95 active:bg-gray-900/10`}
-        >
+            className={`${gradient} flex h-12 items-center gap-4 rounded-full bg-gradient-to-r px-4 text-gray-100  bg-blend-overlay transition-all duration-[0.1s] hover:bg-gray-100/10 hover:shadow-gray-900/50 first-line:hover:shadow active:scale-95 active:bg-gray-900/10`}>
+            <p>this was added on the phone</p>
             {children}
         </button>
-    )
-}
+    );
+};
 
-export default Button
+export default Button;
