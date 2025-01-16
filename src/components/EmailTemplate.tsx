9bf5d0786bd5038e6fc814da3ca360facab44069
Rizwan Ahamad <rizwanahamadcodes@gmail.com>
2025-01-16 14:19:03 +0545

    basic resend setup

---
+++ src/components/EmailTemplate.tsx
@@ -0,0 +1,15 @@
+type EmailTemplateProps = {
+    firstName: string
+}
+
+const EmailTemplate = (props: Readonly<EmailTemplateProps>) => {
+    const { firstName } = props
+
+    return (
+        <div>
+            <h1>Welcome, {firstName}!</h1>
+        </div>
+    )
+}
+
+export default EmailTemplate
