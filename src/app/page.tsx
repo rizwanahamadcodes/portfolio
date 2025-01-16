a16628b4269d1e2bde937bc64da3a126864555dc
Rizwan Ahamad <rizwanahamadcodes@gmail.com>
2025-01-16 14:18:47 +0545

    added dark mode scaffolding, no switch components or styles, just the scaffolding

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,11 @@
+import styles from "../styles/main.module.scss";
+import Header from "./Header";
+
 export default function Home() {
-    return <h1>Hi!</h1>;
+    return (
+        <>
+            <Header />
+            <h1 className={styles["hi-there"]}>Hi! i was styled by scss</h1>
+        </>
+    );
 }
