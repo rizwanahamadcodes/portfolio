c365cebbd91d13283c77bba2a3e052a693637948
Rizwan Ahamad <rizwanahamadcodes@gmail.com>
2025-01-16 14:19:12 +0545

    live demo link was in the wrong place

--- src/components/ProjectsSection/ProjectsSection.tsx
+++ src/components/ProjectsSection/ProjectsSection.tsx
@@ -17,8 +17,8 @@ const projects = [
         projectDesc:
             "Feature rich image editor with additional capability to render images based on huge dataset, templates once created using the available fields can be used to generate multiples design with dynamic content",
         projectImageSrc: "/img/cardgen.jpg",
-        githubLink: "https://cardgen-seven.vercel.app/projects/1/edit",
-        liveDemoLink: "https://cardgen-seven.vercel.app/",
+        githubLink: "https://github.com/rizwanahamadcodes/cardgen",
+        liveDemoLink: "https://cardgen-seven.vercel.app/projects/1/edit",
     },
 
     {
