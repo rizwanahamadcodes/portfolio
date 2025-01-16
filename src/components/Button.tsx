d6d4935f719b4aaaa5b76ae26be8c97402401a8e
Rizwan Ahamad <rizwanahamadcodes@gmail.com>
2025-01-16 14:18:54 +0545

    Added the Props type declaration to Button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,7 +1,12 @@
 import React from 'react'
 import { FaPhoneAlt } from 'react-icons/fa'
 
-const Button = ({ children, gradient }) => {
+interface ButtonProps {
+    children: React.ReactNode
+    gradient: string
+}
+
+const Button: React.FC<ButtonProps> = ({ children, gradient }) => {
     return (
         <button
             className={`${gradient} flex h-12 items-center gap-4 rounded-full bg-gradient-to-r px-4 text-gray-100  bg-blend-overlay transition-all hover:bg-gray-100/10 hover:shadow hover:shadow-gray-900/50 active:bg-gray-900/10`}
