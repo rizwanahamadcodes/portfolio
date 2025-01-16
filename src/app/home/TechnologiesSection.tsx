2b2f3ea18cb2a54b221ac6c43eb32d8e3f86e693
Rizwan Ahamad <rizwanahamadcodes@gmail.com>
2025-01-16 14:18:59 +0545

    technologies section starting to take shape

--- src/app/home/TechnologiesSection.tsx
+++ src/app/home/TechnologiesSection.tsx
@@ -1,9 +1,108 @@
 import Section from '@/components/Section'
-import React from 'react'
-import logo from '/public/img/rizwan_logo__primary-to-primary-support-gradient.svg'
-import Image from 'next/image'
-import { DiJavascript1, DiSass, DiCss3 } from 'react-icons/di'
-import { FaCss3 } from 'react-icons/fa6'
+import { CgFigma } from 'react-icons/cg'
+import { DiCss3, DiHtml5, DiJavascript, DiReact, DiSass } from 'react-icons/di'
+import {
+    SiAdobeillustrator,
+    SiAdobephotoshop,
+    SiBootstrap,
+    SiJavascript,
+    SiNextdotjs,
+    SiTailwindcss,
+} from 'react-icons/si'
+
+const techList = [
+    {
+        id: 1,
+        title: 'Javascript',
+        categoryId: 1,
+        icon: DiJavascript,
+        color: '#f7df1e',
+    },
+    {
+        id: 2,
+        title: 'CSS',
+        categoryId: 1,
+        icon: DiCss3,
+        color: '#2965f1',
+    },
+    {
+        id: 3,
+        title: 'HTML',
+        categoryId: 1,
+        icon: DiHtml5,
+        color: '#e34f26',
+    },
+    {
+        id: 4,
+        title: 'NextJS',
+        categoryId: 2,
+        icon: SiNextdotjs,
+        color: '#000',
+    },
+    {
+        id: 5,
+        title: 'ReactJS',
+        categoryId: 2,
+        icon: DiReact,
+        color: '#00d8ff',
+    },
+    {
+        id: 6,
+        title: 'Tailwind',
+        categoryId: 2,
+        icon: SiTailwindcss,
+        color: '#00b4b6',
+    },
+    {
+        id: 7,
+        title: 'Photoshop',
+        categoryId: 3,
+        icon: SiAdobephotoshop,
+        color: '#100f27',
+    },
+    {
+        id: 8,
+        title: 'Illustrator',
+        categoryId: 3,
+        icon: SiAdobeillustrator,
+        color: '#261300',
+    },
+    {
+        id: 9,
+        title: 'Figma',
+        categoryId: 3,
+        icon: CgFigma,
+        color: '#0acf84',
+    },
+    {
+        id: 10,
+        title: 'Bootstrap',
+        categoryId: 2,
+        icon: SiBootstrap,
+        color: '#8911fb',
+    },
+    {
+        id: 11,
+        title: 'Sass',
+        categoryId: 2,
+        icon: DiSass,
+        color: '#c69',
+    },
+]
+const techCategoriesList = [
+    {
+        id: 2,
+        title: 'Frameworks and Libraries',
+    },
+    {
+        id: 1,
+        title: 'Programming Languages and Markup',
+    },
+    {
+        id: 3,
+        title: 'Design Tools',
+    },
+]
 
 const TechnologiesSection = () => {
     return (
@@ -13,61 +112,44 @@ const TechnologiesSection = () => {
             title="Technologies I'm familiar with"
             subtitle="I have been programming for six years and these are the technologies i picked up along the way"
         >
-            <div className="mb-8">
-                <h5 className="mb-4 font-medium text-primary dark:text-primary">
-                    Programming and Markup
-                </h5>
-                <div className="flex gap-4">
-                    <div className="flex cursor-pointer items-center gap-3 rounded-full border-[2px] border-transparent bg-gray-100 p-2 pr-4 hover:border-gray-300/80 hover:shadow dark:bg-gray-700 dark:hover:border-gray-900/80">
-                        <div className="rounded-full bg-white p-1 dark:bg-gray-600">
-                            <DiJavascript1
-                                size={30}
-                                className="text-[#f7df1e]"
-                            />
+            <div className="flex flex-col gap-8">
+                {techCategoriesList.map((techCategory) => (
+                    <div key={techCategory.id}>
+                        <h5 className="mb-4 font-medium text-primary dark:text-primary">
+                            {techCategory.title}
+                        </h5>
+
+                        <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5">
+                            {techList
+                                .filter(
+                                    (techItem) =>
+                                        techItem.categoryId === techCategory.id
+                                )
+                                .map((tech) => {
+                                    const { icon: Icon } = tech
+                                    return (
+                                        <div
+                                            key={tech.id}
+                                            className="flex cursor-pointer items-center gap-3 rounded-full border-[2px] border-transparent bg-gray-100 p-2 pr-4 hover:border-gray-300/60 hover:shadow active:scale-[0.98] dark:bg-gray-700 dark:hover:border-gray-900/60"
+                                        >
+                                            <div
+                                                style={{
+                                                    background: tech.color,
+                                                }}
+                                                className="flex items-center justify-center rounded-full bg-white p-2 dark:bg-gray-600"
+                                            >
+                                                <Icon
+                                                    size={30}
+                                                    className="text-white"
+                                                />
+                                            </div>
+                                            <p>{tech.title}</p>
+                                        </div>
+                                    )
+                                })}
                         </div>
-                        <p>Javascript</p>
                     </div>
-                    <div className="flex cursor-pointer items-center gap-3 rounded-full border-[2px]  border-transparent bg-gray-100 p-2 pr-4 hover:border-gray-300/80 hover:shadow dark:bg-gray-700 dark:hover:border-gray-900/80">
-                        <div className="rounded-full bg-white p-1 dark:bg-gray-600">
-                            <DiSass size={30} className="text-[#c69]" />
-                        </div>
-                        <p>Sass</p>
-                    </div>
-                    <div className="flex cursor-pointer items-center gap-3 rounded-full border-[2px] border-transparent bg-gray-100 p-2 pr-4 hover:border-gray-300/80 hover:shadow dark:bg-gray-700 dark:hover:border-gray-900/80">
-                        <div className="rounded-full bg-white p-1 dark:bg-gray-600">
-                            <DiCss3 size={30} className="text-[#264de4]" />
-                        </div>
-                        <p>CSS</p>
-                    </div>
-                </div>
-            </div>{' '}
-            <div>
-                <h5 className="mb-4 font-medium text-primary dark:text-primary">
-                    Design
-                </h5>
-                <div className="flex gap-4">
-                    <div className="flex cursor-pointer items-center gap-3 rounded-full border-[2px] border-transparent bg-gray-100 p-2 pr-4 hover:border-gray-300/80 hover:shadow dark:bg-gray-700 dark:hover:border-gray-900/80">
-                        <div className="rounded-full bg-white p-1 dark:bg-gray-600">
-                            <DiJavascript1
-                                size={30}
-                                className="text-[#f7df1e]"
-                            />
-                        </div>
-                        <p>Javascript</p>
-                    </div>
-                    <div className="flex cursor-pointer items-center gap-3 rounded-full border-[2px]  border-transparent bg-gray-100 p-2 pr-4 hover:border-gray-300/80 hover:shadow dark:bg-gray-700 dark:hover:border-gray-900/80">
-                        <div className="rounded-full bg-white p-1 dark:bg-gray-600">
-                            <DiSass size={30} className="text-[#c69]" />
-                        </div>
-                        <p>Sass</p>
-                    </div>
-                    <div className="flex cursor-pointer items-center gap-3 rounded-full border-[2px] border-transparent bg-gray-100 p-2 pr-4 hover:border-gray-300/80 hover:shadow dark:bg-gray-700 dark:hover:border-gray-900/80">
-                        <div className="rounded-full bg-white p-1 dark:bg-gray-600">
-                            <DiCss3 size={30} className="text-[#264de4]" />
-                        </div>
-                        <p>CSS</p>
-                    </div>
-                </div>
+                ))}
             </div>
         </Section>
     )
