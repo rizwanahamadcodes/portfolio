dd3af916113059a0d093e6384cb3c22b03c0c740
Rizwan Ahamad <rizwanahamadcodes@gmail.com>
2025-01-16 14:18:52 +0545

    scafolding of drawer work in the NewDrawerTest file

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,10 +4,20 @@ import Container from '@/components/Container'
 import ThemedImage from '@/components/ThemedImage'
 import heroImageLight from '../../public/img/hero_image_light.svg'
 import heroImageDark from '../../public/img/hero_image_dark.svg'
+import NewDrawer, {
+    DrawerBody,
+    DrawerFoot,
+    DrawerHead,
+} from '@/components/NewDrawer'
 
 export default function Home() {
     return (
         <Container>
+            <NewDrawer>
+                <DrawerHead>I am the drawer head</DrawerHead>
+                <DrawerBody>I am the drawer body</DrawerBody>
+                <DrawerFoot>I am the drawer foot</DrawerFoot>
+            </NewDrawer>
             <h1>This is the home page</h1>
             <p>I am a parapgraph</p>
             <ThemedImage
