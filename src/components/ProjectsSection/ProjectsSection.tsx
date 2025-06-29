529b769ef537fb705490d4089f771bfec2ed3d0f
Rizwan Ahamad <rizwanahamadcodes@gmail.com>
2025-06-29 14:16:34 +0545

    started work on new card

--- src/components/ProjectsSection/ProjectsSection.tsx
+++ src/components/ProjectsSection/ProjectsSection.tsx
@@ -71,7 +71,7 @@ const ProjectsSection = (props: ProjectSectionProps) => {
                     Every project featuring the usage of some of the most
                     important front end tools
                 </SectionSubtitle>
-                <div className="flex max-w-3xl flex-col gap-1">
+                <div className="w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-1">
                     {projects.map((project, index) => (
                         <Project
                             project={project}
@@ -96,63 +96,85 @@ const Project = (props: ProjectProps) => {
     const { project, positionInParent } = props;
 
     return (
-        <div
-            className={clsx(
-                "group relative flex flex-col overflow-hidden rounded-1 bg-white p-0.75 shadow transition-all hover:shadow-md dark:bg-gray-800",
-                positionInParent % 2 === 0
-                    ? "sm:flex-row"
-                    : "sm:flex-row-reverse"
-            )}>
-            <div className="p-1 sm:w-2/4">
-                <Link
-                    href={project.liveDemoLink}
-                    target="_blank"
-                    className="before:inset-0 before:content-[''] before:absolute before:z-10"
-                    rel="noopener noreferrer">
-                    <SectionCategoryTitle>
-                        {project.projectTitle}
-                    </SectionCategoryTitle>
-                </Link>
-                <p className="mb-0.5">{project.projectDesc}</p>
-                <div className="flex flex-col gap-1 lg:flex-row">
-                    <Link
-                        className={button({ className: "!w-full" })}
-                        href={project.liveDemoLink}
-                        target="_blank"
-                        rel="noopener noreferrer">
-                        <ButtonIcon icon={TbWindowMaximize} />
-                        Live Demo
-                    </Link>
-                    <Link
-                        className={button({
-                            variant: "outline",
-                            colorScheme: "themed-gray",
-                            className: "!w-full z-20",
-                        })}
-                        href={project.githubLink}
-                        target="_blank"
-                        rel="noopener noreferrer">
-                        <ButtonIcon icon={FaGithub} />
-                        Code
-                    </Link>
-                </div>
-            </div>
-            <div className="relative sm:w-2/4">
-                <div
-                    className={clsx(
-                        "h-18 transition-all sm:absolute sm:right-[15%] sm:top-[10%] sm:h-full sm:w-[130%]",
-                        positionInParent % 2 === 0
-                            ? "sm:left-[15%] sm:top-[10%] sm:group-hover:left-[10%] sm:group-hover:-rotate-6"
-                            : " sm:group-hover:right-[10%] sm:group-hover:rotate-6"
-                    )}>
+        <div className="bg-white rounded-1 w-full  dark:bg-gray-800 shadow">
+            <div className="px-0.5 pt-0.5">
+                <div className="pb-[calc(calc(9_/_16)_*100%)] bg-red-500 relative">
                     <Image
                         src={project.projectImageSrc}
+                        className="h-full w-full object-cover rounded-0.5"
                         fill
-                        alt=""
-                        className="rounded-0.25 object-cover sm:rounded-1 object-left-top border-black/10 border"
+                        alt={project.projectTitle}
                     />
                 </div>
             </div>
+            <div className="p-1 flex gap-0.5">
+                <div className="text-primary bg-primary-100 px-0.875 py-0.25 text-0.75 rounded-0.5">
+                    HTML
+                </div>
+                <div className="text-primary bg-primary-100 px-0.875 py-0.25 text-0.75 rounded-0.5">
+                    CSS
+                </div>
+            </div>
         </div>
+        // <div className="rounded-1 border">
+        // </div>
+        // <div
+        //     className={clsx(
+        //         "group relative flex flex-col overflow-hidden rounded-1 bg-white p-0.75 shadow transition-all hover:shadow-md dark:bg-gray-800",
+        //         positionInParent % 2 === 0
+        //             ? "sm:flex-row"
+        //             : "sm:flex-row-reverse"
+        //     )}>
+        //     <div className="p-1 sm:w-2/4">
+        //         <Link
+        //             href={project.liveDemoLink}
+        //             target="_blank"
+        //             className="before:inset-0 before:content-[''] before:absolute before:z-10"
+        //             rel="noopener noreferrer">
+        //             <SectionCategoryTitle>
+        //                 {project.projectTitle}
+        //             </SectionCategoryTitle>
+        //         </Link>
+        //         <p className="mb-0.5">{project.projectDesc}</p>
+        //         <div className="flex flex-col gap-1 lg:flex-row">
+        //             <Link
+        //                 className={button({ className: "!w-full" })}
+        //                 href={project.liveDemoLink}
+        //                 target="_blank"
+        //                 rel="noopener noreferrer">
+        //                 <ButtonIcon icon={TbWindowMaximize} />
+        //                 Live Demo
+        //             </Link>
+        //             <Link
+        //                 className={button({
+        //                     variant: "outline",
+        //                     colorScheme: "themed-gray",
+        //                     className: "!w-full z-20",
+        //                 })}
+        //                 href={project.githubLink}
+        //                 target="_blank"
+        //                 rel="noopener noreferrer">
+        //                 <ButtonIcon icon={FaGithub} />
+        //                 Code
+        //             </Link>
+        //         </div>
+        //     </div>
+        //     <div className="relative sm:w-2/4">
+        //         <div
+        //             className={clsx(
+        //                 "h-18 transition-all sm:absolute sm:right-[15%] sm:top-[10%] sm:h-full sm:w-[130%]",
+        //                 positionInParent % 2 === 0
+        //                     ? "sm:left-[15%] sm:top-[10%] sm:group-hover:left-[10%] sm:group-hover:-rotate-6"
+        //                     : " sm:group-hover:right-[10%] sm:group-hover:rotate-6"
+        //             )}>
+        //             <Image
+        //                 src={project.projectImageSrc}
+        //                 fill
+        //                 alt=""
+        //                 className="rounded-0.25 object-cover sm:rounded-1 object-left-top border-black/10 border"
+        //             />
+        //         </div>
+        //     </div>
+        // </div>
     );
 };
