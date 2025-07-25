1ab2c83fd74f172df117b7df913f4d7cdc090214
Rizwan Ahamad <rizwanahamadcodes@gmail.com>
2025-07-25 23:57:49 +0545

    animated the active nav background using framer motion

--- src/components/Navbar/NavMenu/NavMenu.tsx
+++ src/components/Navbar/NavMenu/NavMenu.tsx
@@ -1,6 +1,7 @@
 "use client";
 import { PathConstant } from "@/route/pathConstants";
 import clsx from "clsx";
+import { motion } from "framer-motion";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { IconType } from "react-icons";
@@ -21,30 +22,25 @@ const NavMenu = (props: NavMenuProps) => {
     };
 
     return (
-        <ul
-            className={clsx(
-                "flex",
-                stylesAsPerDirection[direction],
-                className
-            )}>
+        <ul className={clsx("flex", stylesAsPerDirection[direction], className)}>
             {navLinks.map(({ path, label, icon, activeIcon }) => {
                 const isActive = path === pathname;
                 return (
                     <li key={path}>
-                        <Link
-                            href={path}
-                            className={clsx(
-                                "flex h-2.75 px-1.375 rounded-full items-center text-0.875 font-medium capitalize gap-0.5 transition-all duration-300",
-                                isActive
-                                    ? "text-primary-600 bg-primary-100 dark:bg-primary-800 dark:text-primary-200"
-                                    : "text-gray-500 hover:text-gray-500 hover:bg-gray-200 dark:text-gray-300 dark:hover:text-gray-200 dark:hover:bg-gray-800"
-                            )}>
-                            <NavIcon
-                                isActive={isActive}
-                                icon={icon}
-                                activeIcon={activeIcon}
-                            />
-                            {label}
+                        <Link href={path} className={clsx("flex h-2.75 px-1.375 rounded-full items-center text-0.875 font-medium capitalize gap-0.5 transition-all duration-300 relative", isActive ? "text-primary-600 dark:text-primary-200" : "text-gray-500 dark:text-gray-300 hover:text-gray-700 hover:bg-gray-100 dark:hover:text-gray-200 dark:hover:bg-gray-800")}>
+                            {isActive && (
+                                <motion.div
+                                    layoutId={`nav-active-background-${direction}`} // Must be unique if reused elsewhere
+                                    className="absolute h-full w-full bg-primary-100 dark:bg-primary-800 top-0 left-0 rounded-full pointer-events-none z-1"
+                                    transition={{
+                                        type: "spring",
+                                        stiffness: 500,
+                                        damping: 30,
+                                    }}
+                                />
+                            )}
+                            <NavIcon isActive={isActive} icon={icon} activeIcon={activeIcon} className="relative z-2" />
+                            <span className="relative z-2">{label}</span>
                         </Link>
                     </li>
                 );
@@ -58,20 +54,17 @@ export default NavMenu;
 interface NavIconProps {
     isActive: boolean;
     icon?: IconType;
+    className?: string;
     activeIcon?: IconType;
 }
 
-export const NavIcon: React.FC<NavIconProps> = ({
-    isActive,
-    icon: Icon,
-    activeIcon: ActiveIcon,
-}) => {
+export const NavIcon: React.FC<NavIconProps> = ({ className, isActive, icon: Icon, activeIcon: ActiveIcon }) => {
     if (isActive && ActiveIcon) {
-        return <ActiveIcon className="text-1.25" />;
+        return <ActiveIcon className={clsx("text-1.25", className)} />;
     }
 
     if (!isActive && Icon) {
-        return <Icon className="text-1.25" />;
+        return <Icon className={clsx("text-1.25", className)} />;
     }
 
     return null;
